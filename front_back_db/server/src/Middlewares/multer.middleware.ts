e8227821ff21d846e9b2a177d5763d299fb6eddd
volodymyrpankiv <volodymyr.pankiv@teck.com>
2022-10-10 16:30:15 +0300

    added multer middleware

---
+++ front_back_db/server/src/Middlewares/multer.middleware.ts
@@ -0,0 +1,25 @@
+const multer = require('multer')
+
+const DIR = './public/uploads';
+
+const storage = multer.diskStorage({
+  destination: function (_req, _file, cb) {
+    cb(null, DIR)
+  },
+  filename: function (_req, file, cb) {
+    const filename = Date.now() + '-' + file.originalname;
+
+    cb(null, filename);
+  }
+});
+const upload = multer({ storage: storage });
+
+const saveImage = (req, res, next) => {
+  return upload.single('image')(req, res, () => {
+    if (!req.file) return res.json({ error: 'Invalid file type' });
+
+    next();
+  });
+}
+
+export { saveImage };
